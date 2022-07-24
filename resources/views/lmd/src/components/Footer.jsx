82215fb781becbe012eb4db843a9912df574573d
Dhruv <dhruvbehal8@gmail.com>
2022-07-24 16:10:02 -0400

    added reciever View
    added reciever view

---
+++ resources/views/lmd/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <footer class="py-3 bg-primary fixed-bottom">
+        <div class="container">
+          <p class="m-0 text-center text-white">
+            Copyright &copy; Last Mile Delivery
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
